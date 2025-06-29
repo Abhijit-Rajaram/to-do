0edf4484435a7e3a9490c28c411d384b33017d40
Abhijit-Rajaram <abhijitrajaram.developer@gmail.com>
2025-06-29 19:26:39 +0530

    added a failing test

--- app/tests.py
+++ app/tests.py
@@ -5,3 +5,7 @@ class SimpleTest(TestCase):
     def test_user_creation(self):
         user = User.objects.create_user(username="ci_user", password="pass")
         self.assertEqual(user.username, "ci_user")
+
+class FailingTest(TestCase):
+    def test_fail_intentionally(self):
+        self.assertEqual(1, 0, "This test is supposed to fail and break CI")
