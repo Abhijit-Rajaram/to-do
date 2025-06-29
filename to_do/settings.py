e1742f0f03025dc3ae2767d63fe940a3367a8458
Abhijit-Rajaram <abhijitrajaram.developer@gmail.com>
2025-06-29 19:46:15 +0530

    added a fall back secret key so that django won't crash

--- to_do/settings.py
+++ to_do/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
+SECRET_KEY = os.getenv('SECRET_KEY', 'wX3UJRYiMG6Vsm8TFf1WrAqSeCjXy2rLtDu5bAInKuA')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
